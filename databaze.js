32790c75faa028886df6bb41fd6513b110657017
Veronika S <ver.svbd@gmail.com>
2020-06-01 20:40:48 +0200

    přidána databáze prvních 6 kin

---
+++ databaze.js
@@ -0,0 +1,60 @@
+spev
+
+
+
+const cinemaList = [
+
+  {
+    name: "Kino Světozor",
+    address: "Vodičkova 791, Praha 1",
+    specialProgramme: true,
+    barrierFree: false,
+    bar: true,
+    transport: "metro: Můstek - linka A/B, tramvaj. zastávka: Vodičkova - linka: 3, 5, 6, 9, 14, 24"
+  },
+
+  {
+    name: "Lucerna",
+    address: "Vodičkova 704, Praha 1",
+    specialProgramme: true,
+    barrierFree: false,
+    bar: true,
+    transport: "metro: Můstek - linka A/B, tramvaj.zastávka: Vodičkova - linka: 3, 5, 6, 9, 14, 24"
+  },
+
+  {
+    name: "Bio Oko",
+    address: "Františka Křížka 460/15, Praha 7",
+    specialProgramme: true,
+    barrierFree: true,
+    bar: true,
+    transport: "tramvaj. zastávka: Kamenická - linka: 1, 8, 12, 25, 26"
+  },
+
+  {
+    name: "Kino Aero",
+    address: "Biskupcova 31, Praha 3",
+    specialProgramme: true,
+    barrierFree: true,
+    bar: true,
+    transport: "tramvaj. zastávky: Biskupcova - linka: 9, 10, 11, 16; Ohrada - linka: 1, 9, 11, autobus č. 133, 136 207"
+  },
+
+  {
+    name: "Kino Pilotů",
+    address: "Donská 108, Praha 10",
+    specialProgramme: true,
+    barrierFree: false,
+    bar: false,
+    transport: "tramvaj. zastávka: Krymská - linka: 4, 22, autobus č. 135"
+  },
+
+  {
+    name: "Přítomnost",
+    address: "Siwiecova 1, Praha 3",
+    specialProgramme: false,
+    barrierFree: false,
+    bar: true,
+    transport: "tramvaj. zastávka: Husinecká - linka: 5, 9, 15, 26"
+  },
+]
