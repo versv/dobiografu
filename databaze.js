24d502cefa30f0f5df96631d2ccadecd2c74c961
magdacislerova <55711011+magdacislerova@users.noreply.github.com>
2020-06-09 14:17:43 +0200

    css kin, doplnění databáze

--- databaze.js
+++ databaze.js
@@ -7,7 +7,11 @@ const cinemaList = [
   {
     name: "Kino Světozor",
     address: "Vodičkova 791, Praha 1",
+    tagline: "Domov slavného Kinoautomatu a bývalý kabaret. Kankán se tu už netancuje, ale atmosféra je pořád špičková.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Divadlo v kině - NT Live, The Royal Shakespeare Company", "Opera v kině - The Met: Live in HD", "Bio Senior", "Umění v kině", "Catch-up - English friendly", "Řada různých festivalů"
+    ],
     barrierFree: false,
     bar: true,
     transport: "metro: Můstek - linka A/B, tramvaj. zastávka: Vodičkova - linka: 3, 5, 6, 9, 14, 24",
@@ -31,7 +35,11 @@ const cinemaList = [
   {
     name: "Lucerna",
     address: "Vodičkova 704, Praha 1",
+    tagline: "Elegance sama. V secesním kinosále si připadáte jako v divadle. V malém sále vám zase bude dělat společnost James Dean.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Výstavy na plátnech kin", "Pro seniory", "Cestovatelské kino", "Balet v kině - Bolšoj balet", "Ladies Movie Night", "English friendly", "Řada různých festivalů"
+    ],
     barrierFree: false,
     bar: true,
     transport: "metro: Můstek - linka A/B, tramvaj.zastávka: Vodičkova - linka: 3, 5, 6, 9, 14, 24",
@@ -59,7 +67,11 @@ const cinemaList = [
   {
     name: "Bio Oko",
     address: "Františka Křížka 460/15, Praha 7",
+    tagline: "Oblíbené nejen letenskými hipstery. Je libo shlédnout film z pohodlí staré karosérie nebo plážového lehátka?",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Divadlo v kině - NT Live", "Opera v kině - The Met: Live in HD", "Bio Senior", "Malé oči", "Baby bio", "Filmový klub", "Naslepo", "English friendly", "Řada různých festivalů"
+    ],
     barrierFree: true,
     bar: true,
     transport: "tramvaj. zastávka: Kamenická - linka: 1, 8, 12, 25, 26",
@@ -78,7 +90,11 @@ const cinemaList = [
   {
     name: "Kino Aero",
     address: "Biskupcova 31, Praha 3",
+    tagline: "Nenápadný žižkovský klenot s dvorem ve vnitrobloku, ideálním na pofilmové diskuze nad sklenkou vína.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Divadlo v kině - NT Live", "Opera v kině - The Met: Live in HD", "Bio Senior", "Malé oči", "Baby bio", "Filmový klub", "Naslepo", "Legendy v Aeru", "English friendly", "Řada různých festivalů"
+    ],
     barrierFree: true,
     bar: true,
     transport: "tramvaj. zastávky: Biskupcova - linka: 9, 10, 11, 16; Ohrada - linka: 1, 9, 11, autobus č. 133, 136 207",
@@ -103,7 +119,11 @@ const cinemaList = [
   {
     name: "Kino Pilotů",
     address: "Donská 108, Praha 10",
+    tagline: "I Vršovice už mají své artové kino. S vytříbenou dramaturgií a pohodlím multiplexu.", 
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "English friendly", "Cyklus Citrus", "Řada různých festivalů",
+    ],
     barrierFree: false,
     bar: false,
     transport: "tramvaj. zastávka: Krymská - linka: 4, 22, autobus č. 135",
@@ -111,7 +131,7 @@ const cinemaList = [
       ["Čt", "25.6.2020", "Pocta klasikám road movie - blok krátkáých filmů", "17:30"],
       ["Čt", "25.6.2020", "Na vrchol Velkošvédska", "18:00"],
       ["Čt", "25.6.2020", "Les", "19:00"],
-      ["Čt", "25.6.2020", "Rusko, sních a rock'n'roll", "19:30"],
+      ["Čt", "25.6.2020", "Rusko, sníh a rock'n'roll", "19:30"],
 
     ]
   },
@@ -119,7 +139,9 @@ const cinemaList = [
   {
     name: "Přítomnost",
     address: "Siwiecova 1, Praha 3",
-    specialProgramme: false,
+    tagline: "Funkcionalistický poklad v Domě Radosti s barem přímo v sále. Cashless.",
+    specialProgramme: true,
+    specialProgrammeTypes: ["Divadlo v kině - NT Live", "English friendly"],
     barrierFree: false,
     bar: true,
     transport: "tramvaj. zastávka: Husinecká - linka: 5, 9, 15, 26",
@@ -131,7 +153,9 @@ const cinemaList = [
   {
     name: "Kino Mat",
     address: "Karlovo náměstí 19, Praha 2",
+    tagline: "Opravdu komorní záležitost o 46 sedadlech. Takže lístky radši koupit dopředu.", 
     specialProgramme: true,
+    specialProgrammeTypes: ["Kino za babku", "MET Opera", "Mat hit", "Umění v kině", "Opera v kině - The Met: Live in HD", "Pro děti", "English friendly"],
     barrierFree: false,
     bar: true,
     transport: "metro: Karlovo náměstí, linka B, tramvaj. zastávka: Karlovo náměstí - linka: 2, 3, 6, 14, 18, 22",
@@ -145,7 +169,10 @@ const cinemaList = [
   {
     name: "Edison Filmhub",
     address: "Jeruzalémská 2, Praha 1",
+    tagline: "Tento relativně nedávný přírůstek k artovým kinum v centru sídlí v budově Edisonovy transformační stanice a jde ruku v ruce se stylovou kavárnou.",
     specialProgramme: true,
+    specialProgrammeTypes: [ "Matinee", "Český výběr", "Mentální hygiena", "Movie barf Monday (English friendly)", "Weird Europe", "Řada různých festivalů"
+    ],
     barrierFree: false,
     bar: true,
     transport: "tramvaj. zastávka: Jindřišská - linka: 3, 6, 9, 14, 24",
@@ -160,31 +187,57 @@ const cinemaList = [
   {
     name: "Kino Atlas",
     address: "Sokolovská 1, Praha 8",
+    tagline: "Ideální na rande - v zadní řadě mají útulné dvojsedačky.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Bio Senior", "Bio Junior"
+    ],
     barrierFree: false,
     bar: true,
     transport: "metro: Florenc, linka B/C, tramvaj. zastávka Florenc - linka: 3, 8, 24, Těšnov - linka: 14 ",
     movies: [
-
+      ["Po", "22.6.2020", "#jsemtady", "17:30"],
+      ["Po", "22.6.2020", "Emma", "18:30"],
+      ["Út", "23.6.2020", "V síti", "17:30"],
+      ["Út", "23.6.2020", "Dokonalý pacient", "18:30"],
+      ["Út", "23.6.2020", "Bourák", "20:00"],
+      ["St", "24.6.2020", "Ženská na vrcholu", "14:30"],
+      ["St", "24.6.2020", "V síti: Za školou", "15:30"],
+      ["St", "24.6.2020", "Kalifornský sen", "17:30"],      
+      ["St", "24.6.2020", "Problémissky", "20:00"],
     ]
   },
 
   {
     name: "Komorní kino Evald",
     address: "Národní 28, Praha 1",
+    tagline: "Tady si přijdou na své milovníci severských filmů.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Bio Senior", "Bio Junior", "Severský filmový čtvrtek"
+    ],
     barrierFree: false,
     bar: true,
     transport: "metro: Národní - linka B, Můstek - linka: A/B, tramvaj. zastávka Národní třída - linka: 1, 2, 9, 18, 22",
     movies: [
-
+      ["Po", "22.6.2020", "Sviňa", "18:00"],
+      ["Po", "22.6.2020", "Kalifornský sen", "20:30"],
+      ["Út", "23.6.2020", "Modelář", "14:30"],
+      ["Út", "23.6.2020", "#jsemtady", "17:30"],
+      ["Út", "23.6.2020", "Emma", "20:00"],
+      ["St", "24.6.2020", "V síti", "18:00"],
+      ["St", "24.6.2020", "Bourák", "20:30"],
     ]
   },
 
   {
     name: "Kino Ponrepo",
     address: "Bartolomějská 11, Praha 1",
+    tagline: "Kino Národního filmového archivu, které promítá spoustu nejen českých pokladů.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Činohrení klub 55", "Film před sto lety", "Midnight movies", "Filmy a klima", "Ponrepo dětem", "Paralelní kino"
+    ],
     barrierFree: true, //částečně
     bar: true,
     transport: "metro: Národní, linka: B, tramvaj. zastávka: Národní třída - linka: 1, 2, 9, 18, 22",
@@ -203,14 +256,18 @@ const cinemaList = [
   {
     name: "Kino Dlabačov",
     address: "Bělohorská 24, Praha 6",
+    tagline: "Pokud milujete tvorbu jednoho režiséra, bude se vám líbit cyklus Retrospektivy - nedávno promítali třeba všechny filmy Bratří Coenů.",
     specialProgramme: true,
+    specialProgrammeTypes: [
+      "Divadlo v kine - NT Live", "Kino Senior", "Kino Junior", "Filmový klub", "Kino Junior", "Retrospektivy"
+    ],
     barrierFree: true, //nemůžu to najít, ale myslím si, že je
     bar: true,
     transport: "tramvaj. zastávka: Malovanka - linka: 22, 23, 35, bus: Malovanka - linka: 143, 149 ",
     movies: [
       ["Po", "22.6.2020", "Vlastníci", "18:00"],
       ["Po", "22.6.2020", "V síti", "20:00"],
-      ["Út", "23.6.2020", "Emma.", "18:00"],
+      ["Út", "23.6.2020", "Emma", "18:00"],
       ["Út", "23.6.2020", "Maják", "20:00"],
       ["St", "24.6.2020", "Daleko od Reykjavíku", "18:00"],
       ["St", "24.6.2020", "Afrikou na pionýru", "20:00"],
