923da5f05b8a01eea257ecb07b5459d9b14f3c8d
magdacislerova <55711011+magdacislerova@users.noreply.github.com>
2020-06-20 10:09:04 +0200

    konflikty

--- src/components/MovieByDate.vue
+++ src/components/MovieByDate.vue
@@ -3,15 +3,9 @@
     <div class="programme-day">
       <div class="programme-cinema-name">{{ cinema.name }}</div>
       <div class="movies-list">
-<<<<<<< HEAD
-        <div class="movie">
-          <p>{{title}}</p>
-          <p>{{time}}</p>
-=======
         <div class="movie" v-for="(movie, index) in cinema.movies" v-bind:key="index">
           <p>{{ movie.movieName }}</p>
           <p>{{ movie.time }}</p>
->>>>>>> 02b544096bfe4fb9c6e3982ef1f249ca87d6c6a8
         </div>
       </div>
     </div>
